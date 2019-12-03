2ce7bdc6b4084355c0be8290ed09b727862c5c78
Yotam <yotam.hakim@gmail.com>
2019-12-03 14:58:57 +0200

    0403 Add Name and Avatar to Users, Add New Users to Database

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import firebase from '../../firebase';
+import md5 from 'md5';
 
 class Register extends Component {
 
@@ -12,6 +13,7 @@ class Register extends Component {
         passwordConfirmation: '',
         errors: [],
         loading: false,
+        usersRef: firebase.database().ref('users')
     }
 
     isFormValid = () => {
@@ -61,7 +63,19 @@ class Register extends Component {
                 .createUserWithEmailAndPassword(this.state.email, this.state.password)
                 .then(createdUser => {
                     console.log(createdUser)
-                    this.setState({ loading: false });
+                    createdUser.user.updateProfile({
+                        displayName: this.state.username,
+                        photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`
+                    })
+                        .then(() => {
+                            this.saveUser(createdUser).then(() => {
+                                console.log('user saved');
+                            })
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            this.setState({ errors: this.errors.concat(err), loading: false });
+                        })
                 })
                 .catch(err => {
                     console.log(err);
@@ -70,10 +84,17 @@ class Register extends Component {
         }
     }
 
+    saveUser = createdUser => {
+        return this.state.usersRef.child(createdUser.user.uid).set({
+            name: createdUser.user.displayName,
+            avatar: createdUser.user.photoURL
+        });
+    }
+
     handleInputError = (errors, inputName) => {
         return errors.some(error =>
-            error.message.toLowerCase().includes(inputName)) 
-            ? 'error' 
+            error.message.toLowerCase().includes(inputName))
+            ? 'error'
             : '';
     }
 
