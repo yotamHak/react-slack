098b644e136379713828a3a7d6d3767857fa921d
Yotam <yotam.hakim@gmail.com>
2019-12-03 14:45:44 +0200

    0402 Clear Errors upon Registration, Show Loading State

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -10,7 +10,8 @@ class Register extends Component {
         email: '',
         password: '',
         passwordConfirmation: '',
-        errors: []
+        errors: [],
+        loading: false,
     }
 
     isFormValid = () => {
@@ -50,23 +51,41 @@ class Register extends Component {
     }
 
     handleSubmit = event => {
+        event.preventDefault();
+
         if (this.isFormValid()) {
-            event.preventDefault();
+            this.setState({ errors: [], loading: true });
 
             firebase
                 .auth()
                 .createUserWithEmailAndPassword(this.state.email, this.state.password)
                 .then(createdUser => {
                     console.log(createdUser)
+                    this.setState({ loading: false });
                 })
                 .catch(err => {
                     console.log(err);
+                    this.setState({ errors: this.state.errors.concat(err), loading: false });
                 });
         }
     }
 
+    handleInputError = (errors, inputName) => {
+        return errors.some(error =>
+            error.message.toLowerCase().includes(inputName)) 
+            ? 'error' 
+            : '';
+    }
+
     render() {
-        const { username, email, password, passwordConfirmation, errors } = this.state;
+        const {
+            username,
+            email,
+            password,
+            passwordConfirmation,
+            errors,
+            loading
+        } = this.state;
         const color = "violet";
 
         return (
@@ -76,7 +95,7 @@ class Register extends Component {
                         <Icon name="puzzle piece" color={color} />
                         Register for DevChat
                     </Header>
-                    
+
                     <Form onSubmit={this.handleSubmit} size="large">
                         <Segment stacked>
                             <Form.Input
@@ -87,6 +106,7 @@ class Register extends Component {
                                 placeholder="Username"
                                 onChange={this.handleChange}
                                 value={username}
+                                className={this.handleInputError(errors, 'username')}
                                 type="text" />
 
                             <Form.Input
@@ -97,6 +117,7 @@ class Register extends Component {
                                 placeholder="Email Address"
                                 onChange={this.handleChange}
                                 value={email}
+                                className={this.handleInputError(errors, 'email')}
                                 type="email" />
 
                             <Form.Input fluid
@@ -106,6 +127,7 @@ class Register extends Component {
                                 placeholder="Password"
                                 onChange={this.handleChange}
                                 value={password}
+                                className={this.handleInputError(errors, 'password')}
                                 type="password" />
 
                             <Form.Input fluid
@@ -115,9 +137,12 @@ class Register extends Component {
                                 placeholder="Password Confirmation"
                                 onChange={this.handleChange}
                                 value={passwordConfirmation}
+                                className={this.handleInputError(errors, 'password')}
                                 type="password" />
 
-                            <Button color={color} fluid size="large">Submit</Button>
+                            <Button disabled={loading}
+                                className={loading ? 'loading' : ''}
+                                color={color} fluid size="large">Submit</Button>
                         </Segment>
                     </Form>
 
