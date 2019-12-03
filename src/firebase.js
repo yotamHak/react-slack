e6f35fca48dd78f3fbd4f919640be4962f063aa0
Yotam <yotam.hakim@gmail.com>
2019-12-03 11:50:09 +0200

    added firebase 0203

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyApFdMkmDt5hEhQw1ZQ9IpVz4eBbj6oFiQ",
+    authDomain: "react-slack-da52d.firebaseapp.com",
+    databaseURL: "https://react-slack-da52d.firebaseio.com",
+    projectId: "react-slack-da52d",
+    storageBucket: "react-slack-da52d.appspot.com",
+    messagingSenderId: "449253970443",
+    appId: "1:449253970443:web:b8e0f192eab06a5330ed0e",
+    measurementId: "G-4D8BVW015W"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default "firebase";
