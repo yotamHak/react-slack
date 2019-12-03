4216ceecce4f7c1579d26e82527caa4a50fb59ea
Yotam <yotam.hakim@gmail.com>
2019-12-03 15:07:04 +0200

    0404 Create Login Form and Functionality

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -112,7 +112,7 @@ class Register extends Component {
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450 }}>
-                    <Header as="h2" icon color={color} textAlign="center">
+                    <Header as="h1" icon color={color} textAlign="center">
                         <Icon name="puzzle piece" color={color} />
                         Register for DevChat
                     </Header>
