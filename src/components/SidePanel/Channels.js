412ab72cb07bd9eb72d2395596daa0a0906454f2
Yotam <yotam.hakim@gmail.com>
2019-12-04 12:30:55 +0200

    0705 Put Channel on Global State, Add channel_reducer

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import firebase from '../../firebase';
+import { connect } from 'react-redux';
+import { setCurrentChannel } from '../../actions';
 import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
 
 class Channels extends Component {
@@ -66,11 +68,15 @@ class Channels extends Component {
         this.setState({ [event.target.name]: event.target.value });
     }
 
+    changeChannel = channel => {
+        this.props.setCurrentChannel(channel);
+    }
+
     displayChannels = channels => (
         channels.length > 0 && channels.map(channel => (
             <Menu.Item
                 key={channel.id}
-                onClick={() => console.log(channel)}
+                onClick={() => this.changeChannel(channel)}
                 name={channel.name}
                 style={{ opacity: 0.7 }}
             >
@@ -137,4 +143,7 @@ class Channels extends Component {
     }
 }
 
-export default Channels;
+export default connect(
+    null,
+    { setCurrentChannel }
+)(Channels);
