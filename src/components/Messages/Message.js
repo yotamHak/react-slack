a3b90556bfb07568ba8d7e554fded63d57d2ff45
Yotam <yotam.hakim@gmail.com>
2019-12-04 17:02:59 +0200

    0902 Put Media File in State, Add Mimetype Validation

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import moment from 'moment';
-import { Comment } from 'semantic-ui-react';
+import { Comment, Image } from 'semantic-ui-react';
 
 const isOwnMessage = (message, user) => {
     return message.user.id === user.uid ? 'message__self' : '';
 }
 
+const isImage = (message) => message.hasOwnProperty('image') && !message.hasOwnProperty('content');
+
 const timeFromNow = timestamp => moment(timestamp).fromNow();
 
 const Message = ({ message, user }) => {
-console.log(message)
-console.log(user)
     return (
         <Comment>
             <Comment.Avatar src={message.user.avatar} />
             <Comment.Content className={isOwnMessage(message, user)}>
                 <Comment.Author as="a">{message.user.name}</Comment.Author>
                 <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>
-                <Comment.Text>{message.content}</Comment.Text>
+                {
+                    isImage(message)
+                        ? <Image src={message.image} className="message__image" />
+                        : <Comment.Text>{message.content}</Comment.Text>
+                }
             </Comment.Content>
         </Comment>
     )
