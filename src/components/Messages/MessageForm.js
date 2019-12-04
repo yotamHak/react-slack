efe67a8ea42fd0afdd5509250e515b54aa65fe1e
Yotam <yotam.hakim@gmail.com>
2019-12-04 17:17:41 +0200

    0905 Options for Displaying ProgressBar

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -86,6 +86,7 @@ class MessageForm extends Component {
             () => {
                 this.state.uploadTask.on('state_changed', snap => {
                     const percentUploaded = Math.round((snap.bytesTransferred / snap.totalBytes) * 100);
+                    this.props.isProgressBarVisible(percentUploaded);
                     this.setState({ percentUploaded });
                 },
                     err => {
