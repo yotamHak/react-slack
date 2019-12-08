6fce02a39feee1e75e7fd142d252ef5f8ed47c1d
Yotam <yotam.hakim@gmail.com>
2019-12-08 15:27:05 +0200

    Add Channel Title  Users Count to Messages Header 1001

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -13,7 +13,8 @@ class Messages extends Component {
         messagesLoading: true,
         channel: this.props.currentChannel,
         user: this.props.currentUser,
-        progressBar: false
+        progressBar: false,
+        numUniqueUsers: ''
     }
 
     componentDidMount() {
@@ -37,8 +38,23 @@ class Messages extends Component {
                 this.setState({
                     messages: loadedMessages,
                     messagesLoading: false
-                })
+                });
+
+                this.countUniqueUsers(loadedMessages);
             })
+    }
+
+    countUniqueUsers = messages => {
+        const uniqueUsers = messages.reduce((acc, message) => {
+            if (!acc.includes(message.user.name)) {
+                acc.push(message.user.name);
+            }
+            return acc;
+        }, []);
+
+        const plural = uniqueUsers.length > 1 || uniqueUsers.length === 0;
+        const numUniqueUsers = `${uniqueUsers.length} user${plural ? 's' : ''}`;
+        this.setState({ numUniqueUsers });
     }
 
     displayMessages = messages => (
@@ -57,12 +73,17 @@ class Messages extends Component {
         }
     }
 
+    dislapyChannelName = channel => channel ? `#${channel.name}` : '';
+
     render() {
-        const { messagesRef, messages, channel, user, progressBar } = this.state;
+        const { messagesRef, messages, channel, user, progressBar, numUniqueUsers } = this.state;
 
         return (
             <React.Fragment>
-                <MessagesHeader />
+                <MessagesHeader
+                    channelName={this.dislapyChannelName(channel)}
+                    numUniqueUsers={numUniqueUsers}
+                />
 
                 <Segment>
                     <Comment.Group className={progressBar ? 'messages__progress' : 'messages'}>
