41c00ebdde3ab4a85ec110202d1a0317760bea4d
Yotam <yotam.hakim@gmail.com>
2019-12-09 18:02:43 +0200

    Display Saved User Colors as Icons, Change App Colors on Click 1403

--- src/components/App.js
+++ src/components/App.js
@@ -9,42 +9,46 @@ import SidePanel from './SidePanel/SidePanel';
 import Messages from './Messages/Messages';
 import MetaPanel from './MetaPanel/MetaPanel';
 
-const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
-  <Grid columns="equal" className="app" style={{ background: '#eee' }}>
-    <ColorPanel
-      key={currentUser && currentUser.name}
-      currentUser={currentUser}
-    />
-    <SidePanel
-      key={currentUser && currentUser.uid}
-      currentUser={currentUser}
-    />
-
-    <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages
-        key={currentChannel && currentChannel.id}
-        currentChannel={currentChannel}
+const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts,
+  primaryColor, secondaryColor }) => (
+    <Grid columns="equal" className="app" style={{ background: secondaryColor }}>
+      <ColorPanel
+        key={currentUser && currentUser.name}
         currentUser={currentUser}
-        isPrivateChannel={isPrivateChannel}
       />
-    </Grid.Column>
+      <SidePanel
+        key={currentUser && currentUser.uid}
+        currentUser={currentUser}
+        primaryColor={primaryColor}
+      />
 
-    <Grid.Column width={4}>
-      <MetaPanel
-        key={currentChannel && currentChannel.name}
-        userPosts={userPosts}
-        currentChannel={currentChannel}
-        isPrivateChannel={isPrivateChannel}
-      />
-    </Grid.Column>
-  </Grid>
-)
+      <Grid.Column style={{ marginLeft: 320 }}>
+        <Messages
+          key={currentChannel && currentChannel.id}
+          currentChannel={currentChannel}
+          currentUser={currentUser}
+          isPrivateChannel={isPrivateChannel}
+        />
+      </Grid.Column>
+
+      <Grid.Column width={4}>
+        <MetaPanel
+          key={currentChannel && currentChannel.name}
+          userPosts={userPosts}
+          currentChannel={currentChannel}
+          isPrivateChannel={isPrivateChannel}
+        />
+      </Grid.Column>
+    </Grid>
+  )
 
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
   currentChannel: state.channel.currentChannel,
   isPrivateChannel: state.channel.isPrivateChannel,
   userPosts: state.channel.userPosts,
+  primaryColor: state.colors.primaryColor,
+  secondaryColor: state.colors.secondaryColor,
 })
 
 export default connect(mapStateToProps)(App);
--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import firebase from '../../firebase';
+import { connect } from 'react-redux';
+import { setColors } from '../../actions';
 
 import { Sidebar, Menu, Divider, Button, Icon, Label, Modal, Segment } from 'semantic-ui-react';
 import { SliderPicker } from 'react-color';
@@ -11,7 +13,25 @@ class ColorPanel extends Component {
         secondary: '',
         user: this.props.currentUser,
         usersRef: firebase.database().ref('users'),
+        userColors: [],
     }
+
+    componentDidMount() {
+        if (this.state.user) {
+            this.addListener(this.state.user.uid);
+        }
+    }
+
+    addListener = userId => {
+        let userColors = [];
+        this.state.usersRef
+            .child(`${userId}/colors`)
+            .on('child_added', snap => {
+                userColors.unshift(snap.val());
+                this.setState({ userColors });
+            })
+    }
+
     handleChangePrimary = color => this.setState({ primary: color.hex });
 
     handleChangeSecondary = color => this.setState({ secondary: color.hex });
@@ -31,18 +51,33 @@ class ColorPanel extends Component {
                 secondary
             })
             .then(() => {
-                console.log("colors added");
                 this.closeModal();
             })
-            .catch(err => console.error(err)); 
+            .catch(err => console.error(err));
     }
 
     openModal = () => this.setState({ modal: true });
 
     closeModal = () => this.setState({ modal: false });
 
+    displayUserColors = colors => (
+        colors.length > 0 && colors.map((color, i) => (
+            <React.Fragment key={i}>
+                <Divider />
+                <div
+                    className="color__container"
+                    onClick={() => this.props.setColors(color.primary, color.secondary)}
+                >
+                    <div className="color__square" style={{ background: color.primary }}>
+                        <div className="color__overlay" style={{ background: color.secondary }}></div>
+                    </div>
+                </div>
+            </React.Fragment>
+        ))
+    )
+
     render() {
-        const { modal, primary, secondary } = this.state;
+        const { modal, primary, secondary, userColors } = this.state;
 
         return (
             <Sidebar
@@ -55,6 +90,7 @@ class ColorPanel extends Component {
             >
                 <Divider />
                 <Button icon="add" size="small" color="blue" onClick={this.openModal} />
+                {this.displayUserColors(userColors)}
 
                 {/* Color Picker Modal */}
                 <Modal basic open={modal} onClose={this.closeModal}>
@@ -85,4 +121,4 @@ class ColorPanel extends Component {
     }
 }
 
-export default ColorPanel;
+export default connect(null, { setColors })(ColorPanel);
